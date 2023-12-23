30a938d2ade74468faede98af5ff75680d10e912
RaaayDar <154654181+RaaayDar@users.noreply.github.com>
2023-12-22 23:21:00 -0600

    Add files via upload

---
+++ Hooks.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+using RWCustom;
+using BepInEx;
+using Debug = UnityEngine.Debug;
+using IL.Menu;
+using BepInEx.Logging;
+
+#pragma warning disable CS0618
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace BiggerID;
+
+[BepInPlugin("RaaayDar.BiggerIDrange", "IDs expanded", "2.0.0")]
+public class Hooks : BaseUnityPlugin
+{
+
+    private void OnEnable()
+    {
+        On.RainWorld.OnModsInit += RainWorldOnOnModsInit;
+        options = new UI(this, base.Logger);
+    }
+
+
+    private bool IsInit;
+    private UI options;
+
+    private void RainWorldOnOnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+        base.Logger.LogInfo("Initializing mod");
+        try
+        {
+            if (IsInit) return;
+
+            
+
+            //Your hooks go here
+            On.RainWorldGame.GetNewID += RainWorldGame_GetNewID;
+            On.RainWorldGame.GetNewID_int += RainWorldGame_GetNewID_int;
+            
+            On.RainWorldGame.ShutDownProcess += RainWorldGameOnShutDownProcess;
+            On.GameSession.ctor += GameSessionOnctor;
+            
+
+            MachineConnector.SetRegisteredOI("RaaayDar.BiggerIDrange", this.options);
+
+            IsInit = true;
+
+            base.Logger.LogInfo("Loaded successfully");
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex);
+            base.Logger.LogInfo("pc esplode....");
+            throw;
+        }
+    }
+
+
+
+    //the code
+
+
+    private EntityID RainWorldGame_GetNewID_int(On.RainWorldGame.orig_GetNewID_int orig, RainWorldGame self, int spawner)
+    {
+        return new EntityID(spawner, UnityEngine.Random.Range(-2147483648, 2147483647));
+    }
+
+    private EntityID RainWorldGame_GetNewID(On.RainWorldGame.orig_GetNewID orig, RainWorldGame self)
+    {
+        return new EntityID(-1, UnityEngine.Random.Range(-2147483648, 2147483647));
+        
+    }
+
+
+
+
+
+
+
+    private void RainWorldGameOnShutDownProcess(On.RainWorldGame.orig_ShutDownProcess orig, RainWorldGame self)
+    {
+        orig(self);
+        ClearMemory();
+    }
+    private void GameSessionOnctor(On.GameSession.orig_ctor orig, GameSession self, RainWorldGame game)
+    {
+        orig(self, game);
+        ClearMemory();
+    }
+
+    #region Helper Methods
+
+    private void ClearMemory()
+    {
+        //If you have any collections (lists, dictionaries, etc.)
+        //Clear them here to prevent a memory leak
+        //YourList.Clear();
+    }
+
+  
+    #endregion
+}
---
+++ UI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using BepInEx.Logging;
+using Menu.Remix.MixedUI;
+using Menu.Remix.MixedUI.ValueTypes;
+
+namespace BiggerID
+{
+
+    internal class UI : OptionInterface
+    {
+        private readonly ManualLogSource logger;
+        public UI(Hooks modInstance, ManualLogSource loggerSource)
+        {
+            this.logger = loggerSource;
+
+            ScavIDOne = config.Bind<int>("RaaayDar_ScavIds=One", 0, new ConfigurableInfo("Sets the starting int for Scavengers."));
+            ScavIDTwo = config.Bind<int>("RaaayDar_ScavIds=Two", 0, new ConfigurableInfo("Sets the ending int for Scavengers."));
+        }
+
+        public readonly Configurable<int> ScavIDOne;
+        public readonly Configurable<int> ScavIDTwo;
+
+        private OpLabel scavIdLabelOne;
+        //private OpLabel scavIdLabelTwo;
+        private OpTextBox ScavIDinputOne;
+        // private OpTextBox ScavIDinputtwo;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            // Initialize tab
+            var opTab = new OpTab(this, "Options");
+            this.Tabs = new[]
+            {
+                opTab
+            };
+
+            scavIdLabelOne = new OpLabel(40f, 410f, "Scav id");
+            //scavIdLabelTwo = new OpLabel(40f, 410f, "Scav id");
+
+
+            opTab.AddItems(
+                new OpLabel(10f, 560f, "OPTIONS", true),
+                new OpLabel(40f, 440f, "Scavs use specific id (can cause glitches)"),
+                    scavIdLabelOne,
+                    ScavIDinputOne
+                    );
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            scavIdLabelOne.Show();
+            ScavIDinputOne?.Show();
+
+
+        }
+    }
+}
+
